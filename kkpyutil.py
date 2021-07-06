e1ea9b44982c15b0343774ca4c37d2fe8e219cb9
Beinan Li <li.beinan@gmail.com>
2021-07-06 16:59:19 +0800

    support for single line to exclude

--- kkpyutil.py
+++ kkpyutil.py
@@ -732,6 +732,7 @@ def match_files_except_lines(file1, file2, excluded=None):
     with open(file2) as fp:
         content2 = fp.readlines()
     if excluded:
+        excluded = (excluded) if type(excluded).__name__ == 'int'
         content1 = [cont for c, cont in enumerate(content1) if c not in excluded]
         content2 = [cont for c, cont in enumerate(content2) if c not in excluded]
     return content1 == content2
