e283d422e1895bd8b89c052f28aecfc638e8dcc8
Beinan Li <li.beinan@gmail.com>
2022-03-27 16:17:53 +0800

    added dmg builder

--- kkpyutil.py
+++ kkpyutil.py
@@ -14,6 +14,7 @@ import argparse
 import collections
 import cProfile as profile
 import copy
+import dataclasses
 import difflib
 import fnmatch
 import functools
@@ -29,6 +30,7 @@ import multiprocessing
 import operator
 import os
 import os.path as osp
+import time
 import types
 from os.path import abspath, basename, dirname, expanduser, exists, isfile, join, splitext
 import platform
@@ -2008,6 +2010,71 @@ def codesign(binary, identity=None, overwrite=True):
     run_cmd(['codesign', '-s', identity, '-v', binary])
     # gatekeeper validation
     run_cmd(['spctl', '-a', '-t', 'exec', '-vv', binary])
+
+
+def build_dmg(masterdir, resdir='', dmg='', name=''):
+    """
+    build a DMG installer based on fixed layout, and lazy-include resources
+    - masterdir: content folder holding the app and optionally sub-folder "help"
+    - resdir: asset folder for building dmg only, not part of app content, usually holds background image (bg.png, made by Keynote 4:3, size 1024x768) and volume iconset (app.icns)
+    - dmg: path to output dmg
+    - name: app name, default to use basename of masterdir
+    """
+    validate_platform('Darwin')
+    app = types.SimpleNamespace(name='', helpDir='', license='', bundle='')
+    res = types.SimpleNamespace(background='', icon='', mountVolume='')
+    app.name = name or osp.basename(masterdir)
+    app.bundle = f'{app.name}.app'
+    app.helpDir = osp.join(masterdir, 'help')
+    if osp.isdir(app.helpDir):
+        app.license = osp.join(app.helpDir, 'LICENSE.txt')
+    res.mountVolume = f'{app.name}_installer'
+    if osp.isdir(resdir):
+        res.background = osp.join(resdir, 'bg.png')
+        res.icon = osp.join(resdir, 'app.icns')
+        assert osp.isfile(res.background)
+        assert osp.isfile(res.icon)
+    out_dmg = dmg or osp.join(osp.dirname(masterdir), f'install_{app.name}.dmg'.lower())
+    dmg_tool = shutil.which('create-dmg')
+    if not dmg_tool:
+        raise FileNotFoundError('Missing create-dmg; install and retry')
+    cmd = [dmg_tool,
+           '--volname', res.mountVolume,
+           '--volicon', res.icon,
+           '--icon-size', '90',
+           '--hide-extension', app.bundle, ]
+    if osp.isfile(app.license):
+        cmd += ['--eula', app.license]
+    window_rect = types.SimpleNamespace(x='100', y='0', w='500', h='300')
+    icon_pos = types.SimpleNamespace(x='150', y='110')
+    appdroplink_pos = types.SimpleNamespace(x='350', y='110')
+    help_pos = types.SimpleNamespace(x='', y='')
+    if osp.isdir(app.helpDir):
+        window_rect.w, window_rect.h = '550', '500'
+        icon_pos.x, icon_pos.y = '150', '150'
+        help_pos.x, help_pos.y = '150', '300'
+        appdroplink_pos.x, appdroplink_pos.y = '400', '150'
+    if osp.isfile(res.background):
+        glogger.warning('Found background image; size must be 1024x768 (Keynote 4:3)')
+        cmd += ['--background', res.background]
+        window_rect.w, window_rect.h = '1024', '820'
+        icon_pos.x, icon_pos.y = '362', '378'
+        help_pos.x, help_pos.y = '362', '500'
+        appdroplink_pos.x, appdroplink_pos.y = '650', '378'
+    # must add help after adjusting for background
+    if osp.isdir(app.helpDir):
+        cmd += ['--add-file', 'Help', app.helpDir, help_pos.x, help_pos.y]
+    cmd += ['--window-pos', window_rect.x, window_rect.y,
+            '--window-size', window_rect.w, window_rect.h,
+            '--icon', app.bundle, icon_pos.x, icon_pos.y,
+            '--app-drop-link', appdroplink_pos.x, appdroplink_pos.y, ]
+    cmd += [out_dmg, masterdir]
+    if tmp_dmgs := glob.glob('rw.*.dmg'):
+        for td in tmp_dmgs:
+            os.remove(td)
+    if osp.isfile(out_dmg):
+        os.remove(out_dmg)
+    run_cmd(cmd)
 
 
 def _test():
